27e17f25aedb0ad116a8a4ac7760a14e9a32a5c8
am5800 <am5800@yandex.ru>
2017-04-26 21:36:23 +0200

    Implement all todos

--- src/tools.cpp
+++ src/tools.cpp
@@ -14,7 +14,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   if (estimations.size() != ground_truth.size() || estimations.size() == 0)
     throw std::exception("Estimation and Ground Truth size mismatch");
 
-  VectorXd sum(estimations.front().rows());
+  VectorXd sum = VectorXd::Zero(estimations.front().rows());
   
   for (int i = 0; i < estimations.size(); ++i) {
     VectorXd residual = ground_truth[i] - estimations[i];
@@ -28,20 +28,24 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   MatrixXd result(3, 4);
 
-  float px = x_state(0);
-  float py = x_state(1);
-  float vx = x_state(2);
-  float vy = x_state(3);
+  double px = x_state(0);
+  double py = x_state(1);
+  double vx = x_state(2);
+  double vy = x_state(3);
 
-  float px_py = px * px + py * py;
+  double px_py = px * px + py * py;
   if (fabs(px_py) < 0.00001) throw std::exception("Division by zero!");
 
-  float sqrt_px_py = sqrt(px_py);
+  double sqrt_px_py = sqrt(px_py);
 
   result(0, 0) = px / sqrt_px_py;
   result(0, 1) = py / sqrt_px_py;
+  result(0, 2) = 0;
+  result(0, 3) = 0;
   result(1, 0) = -py / px_py;
   result(1, 1) = px / px_py;
+  result(1, 2) = 0;
+  result(1, 3) = 0;
   result(2, 0) = py * (vx * py - vy*px) / (sqrt_px_py * px_py);
   result(2, 1) = px * (vy*px - vx*py) / (sqrt_px_py * px_py);
   result(2, 2) = px / sqrt_px_py;
